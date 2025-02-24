6b33d9ee75a3af6e0748df19e0bcdd15666fed2c
Shun Sakai <sorairolake@protonmail.ch>
2025-02-25 08:15:02 +0900

    test: Update benches

--- benches/sfc64.rs
+++ benches/sfc64.rs
@@ -13,6 +13,21 @@ use rand_sfc::{
 use test::Bencher;
 
 #[bench]
+fn equality(b: &mut Bencher) {
+    b.iter(|| Sfc64::from_seed(Default::default()) == Sfc64::from_seed(Default::default()));
+}
+
+#[bench]
+fn new(b: &mut Bencher) {
+    b.iter(|| Sfc64::new(u64::default(), u64::default(), u64::default()));
+}
+
+#[bench]
+fn new_u64(b: &mut Bencher) {
+    b.iter(|| Sfc64::new_u64(u64::default()));
+}
+
+#[bench]
 fn next_u32(b: &mut Bencher) {
     let mut rng = Sfc64::from_os_rng();
     b.iter(|| rng.next_u32());
@@ -27,6 +42,11 @@ fn next_u64(b: &mut Bencher) {
 #[bench]
 fn fill_bytes(b: &mut Bencher) {
     let mut rng = Sfc64::from_os_rng();
-    let mut dst = [u8::default(); 128];
+    let mut dst = [u8::default(); 8];
     b.iter(|| rng.fill_bytes(&mut dst));
+}
+
+#[bench]
+fn from_seed(b: &mut Bencher) {
+    b.iter(|| Sfc64::from_seed(Default::default()));
 }
